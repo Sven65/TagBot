944527124c20883072f2aa0cfb12f81d04f8a8d0
Sven65 <Sven65@users.noreply.github.com>
2022-09-02 10:55:21 +0200

    Remove warnings

--- src/util/message.rs
+++ src/util/message.rs
@@ -1,20 +1,6 @@
-use serenity::{model::prelude::interaction::{modal::ModalSubmitInteraction, InteractionResponseType, message_component::MessageComponentInteraction, application_command::ApplicationCommandInteraction}, prelude::Context, Error as SerenityError};
-
-
+use serenity::{model::prelude::interaction::{modal::ModalSubmitInteraction, InteractionResponseType, application_command::ApplicationCommandInteraction}, prelude::Context, Error as SerenityError};
 
 pub async fn send_modal_message (ctx: Context, interaction: ModalSubmitInteraction, content: &str, ephemeral: bool) -> Result<(), SerenityError> {
-	return interaction.create_interaction_response(&ctx.http, |response| {
-		response
-			.kind(InteractionResponseType::ChannelMessageWithSource)
-			.interaction_response_data(|message| {
-				message
-					.content(content)
-					.ephemeral(ephemeral)
-		})
-	}).await;
-}
-
-pub async fn send_component_message (ctx: Context, interaction: MessageComponentInteraction, content: &str, ephemeral: bool) -> Result<(), SerenityError> {
 	return interaction.create_interaction_response(&ctx.http, |response| {
 		response
 			.kind(InteractionResponseType::ChannelMessageWithSource)
