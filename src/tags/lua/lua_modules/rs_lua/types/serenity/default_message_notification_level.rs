84047f17528f308b4a2bf2226fc0340f79cd7452
Sven65 <Sven65@users.noreply.github.com>
2022-09-26 09:50:32 +0200

    Use the LuaEnum macro

--- src/tags/lua/lua_modules/rs_lua/types/serenity/default_message_notification_level.rs
+++ src/tags/lua/lua_modules/rs_lua/types/serenity/default_message_notification_level.rs
@@ -1,34 +1,10 @@
 use rlua::{UserData, MetaMethod, Value, ToLua};
 use serenity::model::guild::DefaultMessageNotificationLevel;
+use tagbot_macros::LuaEnum;
 
 use crate::tags::lua::lua_modules::rs_lua::types::utils::types::ConstructableFrom;
 
 
 /// Wrapper for [`serenity::model::guild::DefaultMessageNotificationLevel`]
-#[derive(Clone)]
+#[derive(Clone, LuaEnum)]
 pub struct TBDefaultMessageNotificationLevel(pub DefaultMessageNotificationLevel);
-
-impl ConstructableFrom<DefaultMessageNotificationLevel> for TBDefaultMessageNotificationLevel {
-	/// Creates a new wrapper
-	/// 
-	/// # Arguments
-	/// * `notification_level` - The serenity DefaultMessageNotificationLevel to wrap
-	fn new(notification_level: DefaultMessageNotificationLevel) -> TBDefaultMessageNotificationLevel {
-		TBDefaultMessageNotificationLevel(notification_level)
-	}
-}
-
-impl UserData for TBDefaultMessageNotificationLevel {
-	fn add_methods<'lua, T: rlua::UserDataMethods<'lua, Self>>(methods: &mut T) {
-		methods.add_meta_method(MetaMethod::ToString, |ctx, this, _: Value| {
-			let quality = match this.0 {
-				DefaultMessageNotificationLevel::All => "All",
-				DefaultMessageNotificationLevel::Mentions => "Mentions",
-				DefaultMessageNotificationLevel::Unknown => "Unknown",
-				_ => "Unknown",
-			};
-
-			Ok(quality.to_lua(ctx)?)
-		});
-	}
-}
--- src/tags/lua/lua_modules/rs_lua/types/serenity/video_quality_mode.rs
+++ src/tags/lua/lua_modules/rs_lua/types/serenity/video_quality_mode.rs
@@ -1,34 +1,10 @@
 use rlua::{UserData, MetaMethod, Value, ToLua};
 use serenity::{model::{prelude::VideoQualityMode}};
+use tagbot_macros::LuaEnum;
 
 use crate::tags::lua::lua_modules::rs_lua::types::utils::types::ConstructableFrom;
 
 
 /// Wrapper for [`serenity::model::id::MessageId`]
-#[derive(Clone)]
+#[derive(Clone, LuaEnum)]
 pub struct TBVideoQualityMode(pub VideoQualityMode);
-
-impl ConstructableFrom<VideoQualityMode> for TBVideoQualityMode {
-	/// Creates a new wrapper
-	/// 
-	/// # Arguments
-	/// * `video_quality_mode` - The serenity VideoQualityMode to wrap
-	fn new(video_quality_mode: VideoQualityMode) -> TBVideoQualityMode {
-		TBVideoQualityMode(video_quality_mode)
-	}
-}
-
-impl UserData for TBVideoQualityMode {
-	fn add_methods<'lua, T: rlua::UserDataMethods<'lua, Self>>(methods: &mut T) {
-		methods.add_meta_method(MetaMethod::ToString, |ctx, this, _: Value| {
-			let quality = match this.0 {
-				VideoQualityMode::Auto => "Auto",
-				VideoQualityMode::Full => "Full",
-				VideoQualityMode::Unknown => "Unknown",
-				_ => "Unknown",
-			};
-
-			Ok(quality.to_lua(ctx)?)
-		});
-	}
-}
