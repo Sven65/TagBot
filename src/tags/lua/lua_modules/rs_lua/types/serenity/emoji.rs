a0a7e5e667fefaa369c2e08f5497660fd5da4f0f
Sven65 <Sven65@users.noreply.github.com>
2022-09-27 09:19:23 +0200

    Emoji fields

--- src/tags/lua/lua_modules/rs_lua/types/serenity/emoji.rs
+++ src/tags/lua/lua_modules/rs_lua/types/serenity/emoji.rs
@@ -1,6 +1,10 @@
 use rlua::{UserData, MetaMethod, ToLua, Value};
 use serenity::model::prelude::Emoji;
 
+use crate::tags::lua::lua_modules::rs_lua::types::utils::functions::{convert_type, convert_constructable, convert_constructable_option};
+
+use super::{emoji_id::TBEmojiId, user::TBUser};
+
 #[derive(Clone)]
 pub struct TBEmoji(pub Emoji);
 
@@ -9,6 +13,20 @@ impl UserData for TBEmoji {
         methods.add_meta_method(MetaMethod::ToString, |ctx, this, _: Value| {
 			Ok(this.0.to_string().to_lua(ctx)?)
 		});
+
+        methods.add_meta_method(MetaMethod::Index, |ctx, this, value: String| {
+			Ok(match &value.as_str() {
+				&"animated" => convert_type(this.0.animated, ctx)?,
+				&"available" => convert_type(this.0.available, ctx)?,
+				&"id" => convert_constructable::<TBEmojiId, _>(this.0.id, ctx)?,
+				&"name" => convert_type(this.0.name, ctx)?,
+				&"managed" => convert_type(this.0.managed, ctx)?,
+				&"require_colons" => convert_type(this.0.require_colons, ctx)?,
+				// &"roles" => convert_type(this.0.roles, ctx)?,
+				&"user" => convert_constructable_option::<TBUser, _>(this.0.user, ctx)?,
+				&_ => Value::Nil,
+			})
+		})
     }
 }
 
--- src/tags/lua/lua_modules/rs_lua/types/serenity/emoji_id.rs
+++ src/tags/lua/lua_modules/rs_lua/types/serenity/emoji_id.rs
@@ -1,8 +1,16 @@
 use rlua::{UserData, MetaMethod, Value, ToLua};
 use serenity::model::prelude::{EmojiId};
 
+use crate::tags::lua::lua_modules::rs_lua::types::utils::types::ConstructableFrom;
+
 #[derive(Clone, Hash, PartialEq, Eq)]
 pub struct TBEmojiId(pub EmojiId);
+
+impl ConstructableFrom<EmojiId> for TBEmojiId {
+    fn new(value: EmojiId) -> Self {
+        Self(value)
+    }
+}
 
 impl UserData for TBEmojiId {
     fn add_methods<'lua, T: rlua::UserDataMethods<'lua, Self>>(methods: &mut T) {
