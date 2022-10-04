5b6d3277fae72c76c92d752d8ea197035c11d7ec
Sven65 <Sven65@users.noreply.github.com>
2022-10-04 12:00:51 +0200

    🚨 Lint role id

--- src/tags/lua/lua_modules/rs_lua/types/serenity/id/role_id.rs
+++ src/tags/lua/lua_modules/rs_lua/types/serenity/id/role_id.rs
@@ -1,5 +1,5 @@
-use rlua::{UserData, MetaMethod, Value, ToLua};
-use serenity::model::prelude::{RoleId};
+use rlua::{MetaMethod, ToLua, UserData, Value};
+use serenity::model::prelude::RoleId;
 
 use crate::tags::lua::lua_modules::rs_lua::types::utils::types::ConstructableFrom;
 
@@ -7,21 +7,21 @@ use crate::tags::lua::lua_modules::rs_lua::types::utils::types::ConstructableFro
 pub struct TBRoleId(pub RoleId);
 
 impl ConstructableFrom<RoleId> for TBRoleId {
-    fn new(value: RoleId) -> Self {
-        Self(value)
-    }
+	fn new(value: RoleId) -> Self {
+		Self(value)
+	}
 }
 
 impl UserData for TBRoleId {
-    fn add_methods<'lua, T: rlua::UserDataMethods<'lua, Self>>(methods: &mut T) {
-        methods.add_meta_method(MetaMethod::ToString, |ctx, this, _: Value| {
-			Ok(this.0.to_string().to_lua(ctx)?)
+	fn add_methods<'lua, T: rlua::UserDataMethods<'lua, Self>>(methods: &mut T) {
+		methods.add_meta_method(MetaMethod::ToString, |ctx, this, _: Value| {
+			this.0.to_string().to_lua(ctx)
 		});
-    }
+	}
 }
 
 impl From<RoleId> for TBRoleId {
-    fn from(id: RoleId) -> Self {
-        Self(id)
-    }
+	fn from(id: RoleId) -> Self {
+		Self(id)
+	}
 }
