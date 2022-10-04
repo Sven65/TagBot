2fa556d893304dfae33e44e38db6e51648fc92ca
Sven65 <Sven65@users.noreply.github.com>
2022-10-04 11:35:43 +0200

    🚨 Lint util

--- src/util/error.rs
+++ src/util/error.rs
@@ -1,6 +1,6 @@
 #[macro_export]
 /// Lazy error handler
-/// 
+///
 /// Pass a statement that might produce an error.
 /// If it errors, it'll be printed to the console.
 /// If not, it'll return the result of the statement.
--- src/util/input_field.rs
+++ src/util/input_field.rs
@@ -1,4 +1,7 @@
-use serenity::model::{application::component::{ActionRow, ActionRowComponent}, prelude::component::{InputText, SelectMenu}};
+use serenity::model::{
+	application::component::{ActionRow, ActionRowComponent},
+	prelude::component::{InputText, SelectMenu},
+};
 
 pub trait FindInput {
 	fn find_input(&self, name: &str) -> Option<&InputText>;
@@ -7,35 +10,33 @@ pub trait FindInput {
 
 impl FindInput for Vec<ActionRow> {
 	/// Finds an input by custom id
-	/// 
+	///
 	/// # Arguments
-	/// 
+	///
 	/// * `name` - The custom id of the field to find
-    fn find_input(&self, name: &str) -> Option<&InputText> {
+	fn find_input(&self, name: &str) -> Option<&InputText> {
 		let mut res: Option<&InputText> = None;
 
 		self.iter().for_each(|item| {
 			let components = &item.components;
 
-			components.iter().for_each(|component| {
-				match component {
-					ActionRowComponent::InputText(component) => {
-						if component.custom_id == name {
-							res = Some(component);
-						}
-					},
-					&_ => {},
+			components.iter().for_each(|component| match component {
+				ActionRowComponent::InputText(component) => {
+					if component.custom_id == name {
+						res = Some(component);
+					}
 				}
+				&_ => {}
 			});
 		});
 
-		return res;
-    }
+		res
+	}
 
 	/// Finds a select menu by custom id
-	/// 
+	///
 	/// # Arguments
-	/// 
+	///
 	/// * `name` - The custom id of the select menu to find
 	fn find_select(&self, name: &str) -> Option<&SelectMenu> {
 		let mut res: Option<&SelectMenu> = None;
@@ -43,18 +44,16 @@ impl FindInput for Vec<ActionRow> {
 		self.iter().for_each(|item| {
 			let components = &item.components;
 
-			components.iter().for_each(|component| {
-				match component {
-					ActionRowComponent::SelectMenu(component) => {
-						if component.custom_id.as_ref().unwrap() == name {
-							res = Some(component);
-						}
-					},
-					&_ => {},
+			components.iter().for_each(|component| match component {
+				ActionRowComponent::SelectMenu(component) => {
+					if component.custom_id.as_ref().unwrap() == name {
+						res = Some(component);
+					}
 				}
+				&_ => {}
 			});
 		});
 
-		return res;
+		res
 	}
 }
