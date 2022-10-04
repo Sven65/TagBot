2c24f88cb12cbbe36e6868bd23084331f1f7a37f
Sven65 <Sven65@users.noreply.github.com>
2022-10-04 11:22:09 +0200

    🚨 Lint framework

--- src/commands/framework/mod.rs
+++ src/commands/framework/mod.rs
@@ -13,7 +13,9 @@ use std::collections::HashMap;
 
 use crate::handle_error;
 
-use self::structures::{CommandComponentHandlerFn, CommandExecutorFn, CommandModalHandlerFn, OptionCreatorFn};
+use self::structures::{
+	CommandComponentHandlerFn, CommandExecutorFn, CommandModalHandlerFn, OptionCreatorFn,
+};
 
 const CREATE_COMMANDS: bool = false;
 
@@ -32,9 +34,20 @@ pub struct CommandIndex {
 	context: Option<Context>,
 }
 
+pub struct RegistrationMeta {
+	pub f: CommandExecutorFn,
+	pub desc: Option<String>,
+	pub option_creator: Option<OptionCreatorFn>,
+	pub sends_message: bool,
+	pub modal_handler: Option<CommandModalHandlerFn>,
+	pub component_handler: Option<CommandComponentHandlerFn>,
+}
+
 impl Debug for CommandIndex {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-		f.debug_struct("CommandIndex").field("commands", &self.commands).finish()
+		f.debug_struct("CommandIndex")
+			.field("commands", &self.commands)
+			.finish()
 	}
 }
 
@@ -44,37 +57,38 @@ impl CommandIndex {
 		self.context = Some(ctx);
 	}
 
-	pub async fn register_command(
-		&mut self,
-		name: &str,
-		f: CommandExecutorFn,
-		desc: Option<&str>,
-		option_creator: Option<OptionCreatorFn>,
-		sends_message: bool,
-		modal_handler: Option<CommandModalHandlerFn>,
-		component_handler: Option<CommandComponentHandlerFn>,
-	) {
-		if let None = self.context {
+	pub async fn register_command(&mut self, name: &str, options: RegistrationMeta) {
+		if self.context.is_none() {
 			panic!("[CommandIndex] Unable to register commands: Context is None.");
 		}
 
 		if CREATE_COMMANDS {
-			let created = Command::create_global_application_command(&self.context.as_ref().unwrap().http, |command| {
-				command.name(name).description(desc.unwrap_or("Default description."));
+			let created = Command::create_global_application_command(
+				&self.context.as_ref().unwrap().http,
+				|command| {
+					command.name(name).description(
+						options
+							.desc
+							.unwrap_or_else(|| "Default description.".to_string()),
+					);
 
-				let data = match option_creator {
-					Some(option_creator) => option_creator(command),
-					None => command,
-				};
-
-				data
-			})
+					match options.option_creator {
+						Some(option_creator) => option_creator(command),
+						None => command,
+					}
+				},
+			)
 			.await;
 
 			println!("Created global command {:#?}", created.unwrap().name);
 		}
 
-		let tb_command = TBCommand { executor: f, sends_message, modal_handler, component_handler };
+		let tb_command = TBCommand {
+			executor: options.f,
+			sends_message: options.sends_message,
+			modal_handler: options.modal_handler,
+			component_handler: options.component_handler,
+		};
 
 		self.commands.insert(name.to_string(), tb_command);
 
@@ -83,17 +97,19 @@ impl CommandIndex {
 
 	#[allow(dead_code)]
 	pub async fn remove_command(&mut self, id: CommandId) {
-		if let None = self.context {
+		if self.context.is_none() {
 			panic!("[CommandIndex] Unable to remove commands: Context is None.");
 		}
 
 		handle_error!(
-			Command::delete_global_application_command(&self.context.as_ref().unwrap().http, id).await,
+			Command::delete_global_application_command(&self.context.as_ref().unwrap().http, id)
+				.await,
 			"Failed to delete command"
 		);
 	}
 }
 
 lazy_static! {
-	pub static ref COMMAND_INDEX: Mutex<CommandIndex> = Mutex::new(CommandIndex { commands: HashMap::new(), context: None });
+	pub static ref COMMAND_INDEX: Mutex<CommandIndex> =
+		Mutex::new(CommandIndex { commands: HashMap::new(), context: None });
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,11 +1,12 @@
 pub mod framework;
 #[macro_use]
+#[allow(clippy::module_inception)]
 pub mod commands;
 
-use commands::ping2::ping2;
 use commands::add::add;
+use commands::ping2::ping2;
 
-use self::commands::add::{add_tag_handle_modal, add_options_creator};
+use self::commands::add::{add_options_creator, add_tag_handle_modal};
 use self::commands::delete::{delete, delete_options_creator};
 use self::commands::edit::{edit, edit_options_creator, edit_tag_handle_modal};
 use self::commands::glist::glist;
@@ -14,24 +15,23 @@ use self::commands::raw::{raw, raw_options_creator};
 use self::commands::stealtag::{stealtag, stealtag_options_creator};
 use self::commands::tag::{tag, tag_options_creator};
 use self::commands::tagowner::{tagowner, tagowner_options_creator};
-use self::framework::{COMMAND_INDEX};
-
+use self::framework::{RegistrationMeta, COMMAND_INDEX};
 
 #[allow(dead_code)]
+#[rustfmt::skip]
 pub async fn init_commands() {
-	COMMAND_INDEX.lock().await.register_command("ping2", |data, ctx| Box::pin(ping2(data, ctx)), Some("With desc"), None, false, None, None).await;
-	COMMAND_INDEX.lock().await.register_command("add", |data, ctx| Box::pin(add(data, ctx)), Some("Adds tags"), Some(add_options_creator), true, Some(|interaction, ctx| Box::pin(add_tag_handle_modal(interaction, ctx))), None).await;
-	COMMAND_INDEX.lock().await.register_command("delete", |data, ctx| Box::pin(delete(data, ctx)), Some("Deletes a tag"), Some(delete_options_creator), false, None, None).await;
-	COMMAND_INDEX.lock().await.register_command("edit", |data, ctx| Box::pin(edit(data, ctx)), Some("Edits a tag"), Some(edit_options_creator), false, Some(|interaction, ctx| Box::pin(edit_tag_handle_modal(interaction, ctx))), None).await;
+	COMMAND_INDEX.lock().await.register_command("ping2", RegistrationMeta  { f: |data, ctx| Box::pin(ping2(data, ctx)), desc: Some("With desc".to_string()), option_creator: None, sends_message: false, modal_handler: None, component_handler: None }).await;
+	COMMAND_INDEX.lock().await.register_command("add", RegistrationMeta    { f: |data, ctx| Box::pin(add(data, ctx)), desc: Some("Adds tags".to_string()), option_creator: Some(add_options_creator), sends_message: true, modal_handler: Some(|interaction, ctx| Box::pin(add_tag_handle_modal(interaction, ctx))), component_handler: None }).await;
+	COMMAND_INDEX.lock().await.register_command("delete", RegistrationMeta { f: |data, ctx| Box::pin(delete(data, ctx)), desc: Some("Deletes a tag".to_string()), option_creator: Some(delete_options_creator), sends_message: false, modal_handler: None, component_handler: None}).await;
+	COMMAND_INDEX.lock().await.register_command("edit", RegistrationMeta   { f: |data, ctx| Box::pin(edit(data, ctx)), desc: Some("Edits a tag".to_string()), option_creator: Some(edit_options_creator), sends_message: false, modal_handler: Some(|interaction, ctx| Box::pin(edit_tag_handle_modal(interaction, ctx))), component_handler: None}).await;
 
-	COMMAND_INDEX.lock().await.register_command("glist", |data, ctx| Box::pin(glist(data, ctx)), Some("Gives a list of all tags"), None, true, None, None).await;
-	COMMAND_INDEX.lock().await.register_command("list", |data, ctx| Box::pin(list(data, ctx)), Some("Gives a list of all tags you own"), None, true, None, None).await;
-	COMMAND_INDEX.lock().await.register_command("raw", |data, ctx| Box::pin(raw(data, ctx)), Some("Sends the raw data of a tag"), Some(raw_options_creator), true, None, None).await;
+	COMMAND_INDEX.lock().await.register_command("glist", RegistrationMeta  { f: |data, ctx| Box::pin(glist(data, ctx)), desc: Some("Gives a list of all tags".to_string()), option_creator: None, sends_message: true, modal_handler: None, component_handler: None}).await;
+	COMMAND_INDEX.lock().await.register_command("list", RegistrationMeta   { f: |data, ctx| Box::pin(list(data, ctx)), desc: Some("Gives a list of all tags you own".to_string()), option_creator: None, sends_message: true, modal_handler: None, component_handler: None}).await;
+	COMMAND_INDEX.lock().await.register_command("raw", RegistrationMeta    { f: |data, ctx| Box::pin(raw(data, ctx)), desc: Some("Sends the raw data of a tag".to_string()), option_creator: Some(raw_options_creator), sends_message: true, modal_handler: None, component_handler: None}).await;
 
-	COMMAND_INDEX.lock().await.register_command("stealtag", |data, ctx| Box::pin(stealtag(data, ctx)), Some("Steals a tag"), Some(stealtag_options_creator), false, None, None).await;
+	COMMAND_INDEX.lock().await.register_command("stealtag", RegistrationMeta { f: |data, ctx| Box::pin(stealtag(data, ctx)), desc: Some("Steals a tag".to_string()), option_creator: Some(stealtag_options_creator), sends_message: false, modal_handler: None, component_handler: None}).await;
 	
-	COMMAND_INDEX.lock().await.register_command("tagowner", |data, ctx| Box::pin(tagowner(data, ctx)), Some("Shows who owns a tag"), Some(tagowner_options_creator), false, None, None).await;
+	COMMAND_INDEX.lock().await.register_command("tagowner", RegistrationMeta { f: |data, ctx| Box::pin(tagowner(data, ctx)), desc: Some("Shows who owns a tag".to_string()), option_creator: Some(tagowner_options_creator), sends_message: false, modal_handler: None, component_handler: None}).await;
 	
-	COMMAND_INDEX.lock().await.register_command("tag", |data, ctx| Box::pin(tag(data, ctx)), Some("Executes a tag"), Some(tag_options_creator), true, None, None).await;
-
+	COMMAND_INDEX.lock().await.register_command("tag", RegistrationMeta { f: |data, ctx| Box::pin(tag(data, ctx)), desc: Some("Executes a tag".to_string()), option_creator: Some(tag_options_creator), sends_message: true, modal_handler:  None, component_handler: None}).await;
 }
