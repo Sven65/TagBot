82d766fd712e43278fd7c1b4c39d7786074c4c08
Sven65 <Sven65@users.noreply.github.com>
2022-10-13 17:22:32 +0200

    Convert type doc gen

---
+++ tagbot-macros/src/luadoc/convert_parser.rs
@@ -0,0 +1,51 @@
+use syn::{AngleBracketedGenericArguments, Expr, Path, PathArguments};
+
+use crate::luadoc::parse_path;
+
+fn parse_path_args(path: &Path) -> Vec<syn::PathArguments> {
+	path.segments
+		.clone()
+		.into_iter()
+		.map(|s| s.arguments)
+		.collect()
+}
+
+pub fn get_arguments(expr: Expr) -> Vec<syn::PathArguments> {
+	match expr {
+		Expr::Try(tryexp) => match &*tryexp.expr {
+			Expr::Call(call) => match &*call.func {
+				Expr::Path(path) => {
+					return parse_path_args(&path.path);
+				}
+				_ => panic!("Call func is not path"),
+			},
+			_ => panic!("try expr is not call"),
+		},
+		_ => panic!("Expr is not call"),
+	};
+}
+
+pub fn parse_convert_type(expr: Expr) -> String {
+	let args = get_arguments(expr);
+
+	println!("args {:#?}", args);
+
+	let typ = match args.get(0).unwrap() {
+		syn::PathArguments::AngleBracketed(args) => match args {
+			AngleBracketedGenericArguments { args, .. } => {
+				let first = args.first().unwrap();
+
+				match first {
+					syn::GenericArgument::Type(ty) => match ty {
+						syn::Type::Path(path) => parse_path(&path.path),
+						_ => panic!("Type is not path"),
+					},
+					_ => panic!("Argument is not type"),
+				}
+			}
+		},
+		_ => panic!("Args is not angle"),
+	};
+
+	return typ;
+}
